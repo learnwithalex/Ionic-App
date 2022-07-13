c05eb2c0bb784b7b438f963b79f9def66bdc64b7
learnwithalex <tribezsocial@gmail.com>
2022-07-13 17:03:08 +0100

    Changes

---
+++ photo-gallery/src/pages/App.js
@@ -0,0 +1,4 @@
+
+
+
+    document.getElementById('LoadNews').innerHTML = "<IonCard><img src='https://ionicframework.com/docs/demos/api/card/madison.jpg'/><IonCardHeader><IonCardSubtitle>Destination</IonCardSubtitle><IonCardTitle>Madison, WI</IonCardTitle></IonCardHeader><IonCardContent>Founded in 1829 on an isthmus between Lake Monona and Lake Mendota, Madison was named the capital of theWisconsin Territory in 1836.</IonCardContent></IonCard>";
--- photo-gallery/src/pages/Tab1.tsx
+++ photo-gallery/src/pages/Tab1.tsx
@@ -1,49 +1,83 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-  setupIonicReact
-} from '@ionic/react';
+import React from 'react';
+import { IonContent, IonHeader, IonFooter, IonPage, IonTitle, IonInput, IonToolbar, IonFab, IonFabButton, IonIcon, IonItem, IonMenu, IonRouterOutlet, IonLabel, IonFabList, IonList, IonMenuButton, IonButtons, IonSearchbar, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonCardSubtitle, IonGrid, IonRow, IonCol } from '@ionic/react';
+import { add, settings, share, person, arrowForwardCircle, arrowBackCircle, arrowUpCircle, logoVimeo, logoFacebook, logoInstagram, logoTwitter, downloadOutline } from 'ionicons/icons';
 
-import ExploreContainer from '../components/ExploreContainer';
-import { arrowForward, cogOutline, addOutline, homeOutline, refreshOutline, starOutline} from 'ionicons/icons';
-
-import './Tab1.css';
-
-
-
-
-
+import './App.js';
 
 const Tab1: React.FC = () => {
-  return (
+ return (
+ 
     <IonPage>
+
       <IonHeader>
-        <IonToolbar class="Toolbar">
-            <IonIcon icon={starOutline} />
-          <input type="text" name="url" placeholder="Search Or Enter Address" className='inputToolbar'></input>
-          <IonIcon icon={refreshOutline} />
+        <IonToolbar class="ion-padding-top">
+         <IonSearchbar placeholder="Search or enter Address"></IonSearchbar>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large"></IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonContent>
         
-        <IonContent class="ion-padding tabBody">
-   
-  </IonContent>
+
+
+
+        {/*-- fab placed to the bottom start --*/}
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton>
+            <IonIcon icon={downloadOutline} />
+          </IonFabButton>
+        </IonFab>
+
+       
+        <IonCardHeader>
+          <IonCardTitle class="ion-text-center">
+           <img src="https://questgig.com/questgig.png" height="30" width="30"></img> T-Africa
+          </IonCardTitle>
+        </IonCardHeader>
+      
+<IonGrid>
+  <IonRow class="ion-align-items-start">
+    <IonCol>
+    <IonCard>
+    <IonCardContent>Div</IonCardContent>
+    </IonCard>
+    </IonCol>
+    <IonCol>
+    <IonCard>
+    <IonCardContent>Div</IonCardContent>
+    </IonCard>
+    </IonCol>
+    <IonCol>
+    <IonCard>
+    <IonCardContent>Div</IonCardContent>
+    </IonCard>
+    </IonCol>
+        <IonCol>
+    <IonCard>
+    <IonCardContent>Div</IonCardContent>
+    </IonCard>
+    </IonCol>
+    <IonCol>
+    <IonCard>
+    <IonCardContent>Div</IonCardContent>
+    </IonCard>
+    </IonCol>
+    <IonCol>
+    <IonCard>
+    <IonCardContent>Div</IonCardContent>
+    </IonCard>
+    </IonCol>
+  </IonRow>
+  
+
+</IonGrid>
+<IonCard><img src='https://ionicframework.com/docs/demos/api/card/madison.jpg'/><IonCardHeader><IonCardSubtitle>Destination</IonCardSubtitle><IonCardTitle>Madison, WI</IonCardTitle></IonCardHeader><IonCardContent>Founded in 1829 on an isthmus between Lake Monona and Lake Mendota, Madison was named the capital of theWisconsin Territory in 1836.</IonCardContent></IonCard>
+      {/* <div id="LoadNews">
+
+      </div> */}
         
-      </IonContent>
+     </IonContent>
     </IonPage>
   );
+
 };
 
 export default Tab1;
